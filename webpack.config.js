344e56ba05b99fab41e8f7b259240bea6df8e81d
Michael Cittar <mcittar@gmail.com>
2017-04-16 10:40:01 -0700

    Test bable loading

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         filename: "./lib/bundle.js"
     },
+    module: {
+      loaders: [
+        {
+          test: [/\.jsx?$/, /\.js?$/],
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ]
+    },
     devtool: 'source-maps'
 };
